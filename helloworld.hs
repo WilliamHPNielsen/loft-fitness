0d61522e615d5316cf1558edf66bc2caad28148a
William Nielsen <whpn@mailbox.org>
2019-03-19 19:19:01 +0100

    Make helloworld.hs a small skeleton

--- helloworld.hs
+++ helloworld.hs
@@ -1,2 +1,13 @@
+import Data.Char
+
+hardcorefy x = map toUpper x
+
 main = do
-  putStrLn "Hello, world"
+  putStrLn "Input training type (press q to quit)"
+  trainingtype <- getLine
+  if trainingtype == "q"
+    then return ()
+    else do
+        let hctt = hardcorefy trainingtype
+        putStrLn ("Registered training type: " ++ hctt)
+        main
