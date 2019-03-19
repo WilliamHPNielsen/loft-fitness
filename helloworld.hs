cec10b261912c2b2173b900cefd958001ff61ffe
William Nielsen <whpn@mailbox.org>
2019-03-19 19:25:36 +0100

    Add an IO action outside main

--- helloworld.hs
+++ helloworld.hs
@@ -1,10 +1,16 @@
 import Data.Char
 
+hardcorefy :: [Char] -> [Char]
 hardcorefy x = map toUpper x
+
+getLine' :: IO String
+getLine' = do
+  tt <- getLine
+  return tt
 
 main = do
   putStrLn "Input training type (press q to quit)"
-  trainingtype <- getLine
+  trainingtype <- getLine'
   if trainingtype == "q"
     then return ()
     else do
