80f71511a432b9e0579be1ce43ca0be7045a3ba9
William Nielsen <whpn@mailbox.org>
2019-03-23 15:59:58 +0100

    Rename main function

---
+++ main.hs
@@ -0,0 +1,18 @@
+import Data.Char
+import Control.Monad
+
+hardcorefy :: [Char] -> [Char]
+hardcorefy x = map toUpper x
+
+getLine' :: IO String
+getLine' = do
+  tt <- getLine
+  return tt
+
+main = do
+  putStrLn "Input training type (press Q to quit)"
+  trainingtype <- getLine'
+  when (not $ elem trainingtype ["q", "Q"]) $ do
+    let hctt = hardcorefy trainingtype
+    putStrLn ("Registered training type: " ++ hctt)
+    main
