246cf764a1d04e7e1e5d3e441fda3755e8dc9584
William Nielsen <whpn@mailbox.org>
2019-03-23 20:38:52 +0100

    Include file I/O in session module

--- NewSession.hs
+++ NewSession.hs
@@ -1,7 +1,32 @@
 module NewSession
-  ( startNewSession
+  ( Session
+  , startNewSession
   ) where
+
+import Data.Time
+import System.IO
+
+data Session = Session { time :: ZonedTime
+                       , kind :: String }
+               deriving Show
+
+makeSession :: String -> IO Session
+makeSession kind = do
+  time <- getZonedTime
+  let ses = Session {time=time, kind=kind}
+  return ses
+
+writeSession :: Session -> IO ()
+writeSession ses = do
+  appendFile "sessions.log" (show ses ++ "\n")
 
 startNewSession :: IO ()
 startNewSession = do
   putStrLn "New session, no depression"
+  putStrLn "Enter session type"
+  kind <- getLine
+  ses <- makeSession kind
+  writeSession ses
+  print ses
+  return ()
+  
